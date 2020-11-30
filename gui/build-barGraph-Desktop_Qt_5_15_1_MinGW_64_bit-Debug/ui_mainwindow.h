d53dff69d51136ac8e05d29d405ac93890d80f94
ElanBibas <elanb@visaplace.com>
2020-11-30 16:52:45 -0500

    fixed resizing for a constant size

--- gui/build-barGraph-Desktop_Qt_5_15_1_MinGW_64_bit-Debug/ui_mainwindow.h
+++ gui/build-barGraph-Desktop_Qt_5_15_1_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -15,7 +15,6 @@
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QSpinBox>
 #include <QtWidgets/QStatusBar>
@@ -39,16 +38,21 @@ public:
     QSpinBox *spinBox_2;
     QPushButton *complete;
     QWidget *widget;
-    QMenuBar *menuBar;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(1500, 1000);
-        MainWindow->setMinimumSize(QSize(900, 900));
-        MainWindow->setMaximumSize(QSize(1500, 1000));
+        MainWindow->resize(918, 1000);
+        QSizePolicy sizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
+        MainWindow->setSizePolicy(sizePolicy);
+        MainWindow->setMinimumSize(QSize(918, 1000));
+        MainWindow->setMaximumSize(QSize(900, 900));
+        MainWindow->setStyleSheet(QString::fromUtf8("background-color: rgb(150, 131, 236);"));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
         verticalLayout = new QVBoxLayout(centralWidget);
@@ -60,42 +64,39 @@ public:
         horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
         label_3 = new QLabel(centralWidget);
         label_3->setObjectName(QString::fromUtf8("label_3"));
-        QSizePolicy sizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
-        sizePolicy.setHorizontalStretch(0);
-        sizePolicy.setVerticalStretch(0);
-        sizePolicy.setHeightForWidth(label_3->sizePolicy().hasHeightForWidth());
-        label_3->setSizePolicy(sizePolicy);
+        QSizePolicy sizePolicy1(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(label_3->sizePolicy().hasHeightForWidth());
+        label_3->setSizePolicy(sizePolicy1);
 
         horizontalLayout_2->addWidget(label_3);
 
         alg_comboBox = new QComboBox(centralWidget);
         alg_comboBox->setObjectName(QString::fromUtf8("alg_comboBox"));
-        QSizePolicy sizePolicy1(QSizePolicy::Preferred, QSizePolicy::Fixed);
-        sizePolicy1.setHorizontalStretch(0);
-        sizePolicy1.setVerticalStretch(0);
         sizePolicy1.setHeightForWidth(alg_comboBox->sizePolicy().hasHeightForWidth());
         alg_comboBox->setSizePolicy(sizePolicy1);
+        alg_comboBox->setStyleSheet(QString::fromUtf8("background-color: rgb(247, 184, 1);"));
         alg_comboBox->setMaxVisibleItems(15);
 
         horizontalLayout_2->addWidget(alg_comboBox);
 
         startButton = new QPushButton(centralWidget);
         startButton->setObjectName(QString::fromUtf8("startButton"));
-        QSizePolicy sizePolicy2(QSizePolicy::Minimum, QSizePolicy::Fixed);
-        sizePolicy2.setHorizontalStretch(0);
-        sizePolicy2.setVerticalStretch(0);
-        sizePolicy2.setHeightForWidth(startButton->sizePolicy().hasHeightForWidth());
-        startButton->setSizePolicy(sizePolicy2);
+        sizePolicy1.setHeightForWidth(startButton->sizePolicy().hasHeightForWidth());
+        startButton->setSizePolicy(sizePolicy1);
+        startButton->setStyleSheet(QString::fromUtf8("background-color: rgb(241, 135, 1);"));
 
         horizontalLayout_2->addWidget(startButton);
 
         spinBox = new QSpinBox(centralWidget);
         spinBox->setObjectName(QString::fromUtf8("spinBox"));
-        sizePolicy.setHeightForWidth(spinBox->sizePolicy().hasHeightForWidth());
-        spinBox->setSizePolicy(sizePolicy);
+        sizePolicy1.setHeightForWidth(spinBox->sizePolicy().hasHeightForWidth());
+        spinBox->setSizePolicy(sizePolicy1);
         QFont font;
         font.setPointSize(7);
         spinBox->setFont(font);
+        spinBox->setStyleSheet(QString::fromUtf8("background-color: rgb(247, 184, 1);"));
         spinBox->setAccelerated(true);
         spinBox->setMinimum(0);
         spinBox->setMaximum(10000);
@@ -105,16 +106,17 @@ public:
 
         setDelay = new QPushButton(centralWidget);
         setDelay->setObjectName(QString::fromUtf8("setDelay"));
-        sizePolicy.setHeightForWidth(setDelay->sizePolicy().hasHeightForWidth());
-        setDelay->setSizePolicy(sizePolicy);
+        sizePolicy1.setHeightForWidth(setDelay->sizePolicy().hasHeightForWidth());
+        setDelay->setSizePolicy(sizePolicy1);
         setDelay->setFont(font);
+        setDelay->setStyleSheet(QString::fromUtf8("background-color: rgb(241, 135, 1);"));
 
         horizontalLayout_2->addWidget(setDelay);
 
         label = new QLabel(centralWidget);
         label->setObjectName(QString::fromUtf8("label"));
-        sizePolicy.setHeightForWidth(label->sizePolicy().hasHeightForWidth());
-        label->setSizePolicy(sizePolicy);
+        sizePolicy1.setHeightForWidth(label->sizePolicy().hasHeightForWidth());
+        label->setSizePolicy(sizePolicy1);
         label->setMaximumSize(QSize(16777215, 15));
         label->setLayoutDirection(Qt::LeftToRight);
         label->setAlignment(Qt::AlignCenter);
@@ -123,9 +125,10 @@ public:
 
         spinBox_2 = new QSpinBox(centralWidget);
         spinBox_2->setObjectName(QString::fromUtf8("spinBox_2"));
-        sizePolicy.setHeightForWidth(spinBox_2->sizePolicy().hasHeightForWidth());
-        spinBox_2->setSizePolicy(sizePolicy);
+        sizePolicy1.setHeightForWidth(spinBox_2->sizePolicy().hasHeightForWidth());
+        spinBox_2->setSizePolicy(sizePolicy1);
         spinBox_2->setFont(font);
+        spinBox_2->setStyleSheet(QString::fromUtf8("background-color: rgb(247, 184, 1);"));
         spinBox_2->setAccelerated(true);
         spinBox_2->setMinimum(2);
         spinBox_2->setMaximum(900);
@@ -135,8 +138,10 @@ public:
 
         complete = new QPushButton(centralWidget);
         complete->setObjectName(QString::fromUtf8("complete"));
-        sizePolicy.setHeightForWidth(complete->sizePolicy().hasHeightForWidth());
-        complete->setSizePolicy(sizePolicy);
+        complete->setEnabled(false);
+        sizePolicy1.setHeightForWidth(complete->sizePolicy().hasHeightForWidth());
+        complete->setSizePolicy(sizePolicy1);
+        complete->setStyleSheet(QString::fromUtf8("background-color: rgb(241, 135, 1);"));
 
         horizontalLayout_2->addWidget(complete);
 
@@ -145,18 +150,14 @@ public:
 
         widget = new QWidget(centralWidget);
         widget->setObjectName(QString::fromUtf8("widget"));
-        sizePolicy.setHeightForWidth(widget->sizePolicy().hasHeightForWidth());
-        widget->setSizePolicy(sizePolicy);
-        widget->setMinimumSize(QSize(700, 700));
+        sizePolicy1.setHeightForWidth(widget->sizePolicy().hasHeightForWidth());
+        widget->setSizePolicy(sizePolicy1);
+        widget->setMinimumSize(QSize(900, 900));
         widget->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
 
         verticalLayout->addWidget(widget);
 
         MainWindow->setCentralWidget(centralWidget);
-        menuBar = new QMenuBar(MainWindow);
-        menuBar->setObjectName(QString::fromUtf8("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 1500, 21));
-        MainWindow->setMenuBar(menuBar);
         statusBar = new QStatusBar(MainWindow);
         statusBar->setObjectName(QString::fromUtf8("statusBar"));
         MainWindow->setStatusBar(statusBar);
